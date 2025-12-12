886c35c131cb9bfc26f8fa247db3beb4a1afca47
61903696-dot <61903696@ntc.edu.ph>
2025-12-12 15:28:56 +0800

    Update stats.h

--- assessments/m1/stats.h
+++ assessments/m1/stats.h
@@ -21,20 +21,90 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+#include <stdio.h> // Include standard IO library for use in C files
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints the statistics of an array.
  *
- * <Add Extended Description Here>
+ * This function calls find_minimum, find_maximum, find_mean, and
+ * find_median, and then prints all results to the screen.
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @param test_array The unsigned char pointer to an n-element data array.
+ * @param length The unsigned integer size of the array.
+ * @return No return value (void function).
  */
+void print_statistics(unsigned char *test_array, unsigned int length);
 
+/**
+ * @brief Prints the contents of an array.
+ *
+ * Given an array of data and a length, this function prints the array 
+ * to the screen, nicely formatted with an indicator for the index.
+ *
+ * @param test_array The unsigned char pointer to an n-element data array.
+ * @param length The unsigned integer size of the array.
+ * @return No return value (void function).
+ */
+void print_array(unsigned char *test_array, unsigned int length);
 
-#endif /* __STATS_H__ */
+/**
+ * @brief Finds the median value of the array.
+ *
+ * Given a sorted array of data and a length, this function returns the
+ * median value. The array must be sorted first for this to work correctly.
+ * The result is rounded to the nearest integer.
+ *
+ * @param test_array The unsigned char pointer to an n-element data array.
+ * @param length The unsigned integer size of the array.
+ * @return The unsigned char result of the median calculation.
+ */
+unsigned char find_median(unsigned char *test_array, unsigned int length);
+
+/**
+ * @brief Finds the mean (average) value of the array.
+ *
+ * Given an array of data and a length, this function calculates and
+ * returns the arithmetic mean. The result is rounded to the nearest integer.
+ *
+ * @param test_array The unsigned char pointer to an n-element data array.
+ * @param length The unsigned integer size of the array.
+ * @return The unsigned char result of the mean calculation.
+ */
+unsigned char find_mean(unsigned char *test_array, unsigned int length);
+
+/**
+ * @brief Finds the maximum value in the array.
+ *
+ * Given an array of data and a length, this function returns the maximum value.
+ *
+ * @param test_array The unsigned char pointer to an n-element data array.
+ * @param length The unsigned integer size of the array.
+ * @return The unsigned char maximum value found.
+ */
+unsigned char find_maximum(unsigned char *test_array, unsigned int length);
+
+/**
+ * @brief Finds the minimum value in the array.
+ *
+ * Given an array of data and a length, this function returns the minimum value.
+ *
+ * @param test_array The unsigned char pointer to an n-element data array.
+ * @param length The unsigned integer size of the array.
+ * @return The unsigned char minimum value found.
+ */
+unsigned char find_minimum(unsigned char *test_array, unsigned int length);
+
+/**
+ * @brief Sorts the array from largest to smallest.
+ *
+ * Given an array of data and a length, this function reorders the array
+ * so the zeroth element is the largest value, and the last element (n-1)
+ * is the smallest value.
+ *
+ * @param test_array The unsigned char pointer to an n-element data array.
+ * @param length The unsigned integer size of the array.
+ * @return No return value (void function). The array is sorted in place.
+ */
+void sort_array(unsigned char *test_array, unsigned int length);
+
+#endif // __STATS_H__
